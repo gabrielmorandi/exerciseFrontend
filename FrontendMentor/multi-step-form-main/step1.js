630b0570870541a8a1f25f851bb1d8274cb5980a
Gabriel Morandi de Mello <61758357+gabrielmorandi@users.noreply.github.com>
2022-12-05 04:17:39 -0300

    att teste

--- FrontendMentor/multi-step-form-main/step1.js
+++ FrontendMentor/multi-step-form-main/step1.js
@@ -2,10 +2,8 @@ var form = document.querySelector('form')
 var inputName = document.querySelector('input[type=text]')
 var inputEmail = document.querySelector('input[type=email]')
 var inputTel = document.querySelector('input[type=tel]')
-var urll = 'https://www.google.com'
 
 form.addEventListener('submit', function(e) {
-    e.preventDefault()
     var name = inputName.value
     var email = inputEmail.value
     var tel = inputTel.value
