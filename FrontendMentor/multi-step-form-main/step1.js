fdebde0e8d152e249c3da842ba368df40aeb5e7f
Gabriel Morandi de Mello <61758357+gabrielmorandi@users.noreply.github.com>
2022-12-05 03:34:09 -0300

    att teste

--- FrontendMentor/multi-step-form-main/step1.js
+++ FrontendMentor/multi-step-form-main/step1.js
@@ -4,18 +4,11 @@ var inputEmail = document.querySelector('input[type=email]')
 var inputTel = document.querySelector('input[type=tel]')
 var urll = 'https://www.google.com'
 
-function onpenInNewTab(url) {
-    var win = window.open(url, '_blank')
-    win.focus()
-}
-
 form.addEventListener('submit', function(e) {
     e.preventDefault()
     var name = inputName.value
     var email = inputEmail.value
     var tel = inputTel.value
-
-    onpenInNewTab(urll)
 })
 
 export {name, email, tel}
