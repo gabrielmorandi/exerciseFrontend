5740f5652f051e2ccbe960c21ca3912642dc3416
Gabriel Morandi de Mello <61758357+gabrielmorandi@users.noreply.github.com>
2022-12-11 11:08:41 -0300

    add designo-multi-page-website mobile&tablet versions

---
+++ FrontendMentor/designo-multi-page-website/starter-code/js/menu.js
@@ -0,0 +1,31 @@
+const btnMenu = document.querySelector('.btn-menu')
+const closeBtnMenu = document.querySelector('.close-btn-menu')
+const menu = document.querySelector('.menu')
+const html = document.querySelector('html')
+const links = document.querySelectorAll(".menu li a")
+
+// open menu
+btnMenu.addEventListener('click', () => {
+    menu.classList.toggle('menu-on')
+    btnMenu.classList.add('display-off')
+    closeBtnMenu.classList.add('display-on')
+    html.style.overflow = 'hidden'
+})
+
+// close menu
+closeBtnMenu.addEventListener('click', () => {
+    menu.classList.toggle('menu-on')
+    btnMenu.classList.remove('display-off')
+    closeBtnMenu.classList.remove('display-on')
+    html.style.overflow = 'scroll'
+})
+
+// close menu after link click
+for (let link of links) {
+    link.addEventListener('click', () => {
+        menu.classList.toggle('menu-on')
+        btnMenu.classList.remove('display-off')
+        closeBtnMenu.classList.remove('display-on')
+        html.style.overflow = 'scroll'
+    })
+}
