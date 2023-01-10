492e2241f073aa97150e115e80854c127320454f
Gabriel Morandi de Mello <61758357+gabrielmorandi@users.noreply.github.com>
2023-01-10 09:43:56 -0300

    init product-feedback-app mobile version

---
+++ FrontendMentor/product-feedback-app/starter-code/js/feed.js
@@ -0,0 +1,225 @@
+class DadosMessage {
+    constructor (upvotes, title, description, category, tatalcomments) {
+        this.upvotes = upvotes
+        this.title = title
+        this.description = description
+        this.category = category
+        this.tatalcomments = tatalcomments
+    }
+}
+
+export function attFeed(feed, category, sortBy) {
+    fetch('data.json').then((response) => {
+        response.json().then((dados) => {
+            feed.innerHTML = ''
+            var orgMsg = []
+            dados.productRequests.map((message) => {
+                if (category == 'All') {
+                    if ('comments' in message) {
+                        let totalComments = 0
+                        let comments = message.comments
+                        for (let comment of comments) {
+                            if ('replies' in comment) {
+                                totalComments += comment.replies.length
+                            }
+                        }
+                        totalComments += comments.length
+                        let objMessage = new DadosMessage(message.upvotes, message.title, message.description, message.category, totalComments)
+                        orgMsg.push(objMessage)
+                    } else {
+                        let objMessage = new DadosMessage(message.upvotes, message.title, message.description, message.category, 0)
+                        orgMsg.push(objMessage)
+                    }
+                }
+                else if (category == 'Enhancement') {
+                    if (message.category == 'enhancement') {
+                        if ('comments' in message) {
+                            let totalComments = 0
+                            let comments = message.comments
+                            for (let comment of comments) {
+                                if ('replies' in comment) {
+                                    totalComments += comment.replies.length
+                                }
+                            }
+                            totalComments += comments.length
+                            let objMessage = new DadosMessage(message.upvotes, message.title, message.description, message.category, totalComments)
+                            orgMsg.push(objMessage)
+                        } else {
+                            let objMessage = new DadosMessage(message.upvotes, message.title, message.description, message.category, 0)
+                            orgMsg.push(objMessage)
+                        }
+                    }
+                }
+                else if (category == 'Bug') {
+                    if (message.category == 'bug') {
+                        if ('comments' in message) {
+                            let totalComments = 0
+                            let comments = message.comments
+                            for (let comment of comments) {
+                                if ('replies' in comment) {
+                                    totalComments += comment.replies.length
+                                }
+                            }
+                            totalComments += comments.length
+                            let objMessage = new DadosMessage(message.upvotes, message.title, message.description, message.category, totalComments)
+                            orgMsg.push(objMessage)
+                        } else {
+                            let objMessage = new DadosMessage(message.upvotes, message.title, message.description, message.category, 0)
+                            orgMsg.push(objMessage)
+                        }
+                    }
+                }
+                else if (category == 'Feature') {
+                    if (message.category == 'feature') {
+                        if ('comments' in message) {
+                            let totalComments = 0
+                            let comments = message.comments
+                            for (let comment of comments) {
+                                if ('replies' in comment) {
+                                    totalComments += comment.replies.length
+                                }
+                            }
+                            totalComments += comments.length
+                            let objMessage = new DadosMessage(message.upvotes, message.title, message.description, message.category, totalComments)
+                            orgMsg.push(objMessage)
+                        } else {
+                            let objMessage = new DadosMessage(message.upvotes, message.title, message.description, message.category, 0)
+                            orgMsg.push(objMessage)
+                        }
+                    }
+                }
+                else if (category == 'UI') {
+                    if (message.category == 'ui') {
+                        if ('comments' in message) {
+                            let totalComments = 0
+                            let comments = message.comments
+                            for (let comment of comments) {
+                                if ('replies' in comment) {
+                                    totalComments += comment.replies.length
+                                }
+                            }
+                            totalComments += comments.length
+                            let objMessage = new DadosMessage(message.upvotes, message.title, message.description, message.category, totalComments)
+                            orgMsg.push(objMessage)
+                        } else {
+                            let objMessage = new DadosMessage(message.upvotes, message.title, message.description, message.category, 0)
+                            orgMsg.push(objMessage)
+                        }
+                    }
+                }
+                else if (category == 'UX') {
+                    if (message.category == 'ux') {
+                        if ('comments' in message) {
+                            let totalComments = 0
+                            let comments = message.comments
+                            for (let comment of comments) {
+                                if ('replies' in comment) {
+                                    totalComments += comment.replies.length
+                                }
+                            }
+                            totalComments += comments.length
+                            let objMessage = new DadosMessage(message.upvotes, message.title, message.description, message.category, totalComments)
+                            orgMsg.push(objMessage)
+                        } else {
+                            let objMessage = new DadosMessage(message.upvotes, message.title, message.description, message.category, 0)
+                            orgMsg.push(objMessage)
+                        }
+                    }
+                }
+            })
+            if (sortBy == 'Most Upvotes') {
+                orgMsg.sort((a, b) => {
+                    if (a.upvotes > b.upvotes) return -1
+                    if (a.upvotes < b.upvotes) return 1
+                    return 0
+                })
+            }
+            else if (sortBy == 'Least Upvotes') {
+                orgMsg.sort((a, b) => {
+                    if (a.upvotes > b.upvotes) return 1
+                    if (a.upvotes < b.upvotes) return -1
+                    return 0
+                })
+            }
+            else if (sortBy == 'Most Comments') {
+                orgMsg.sort((a, b) => {
+                    if (a.tatalcomments > b.tatalcomments) return -1
+                    if (a.tatalcomments < b.tatalcomments) return 1
+                    return 0
+                })
+            }
+            else if (sortBy == 'Least Comments') {
+                orgMsg.sort((a, b) => {
+                    if (a.tatalcomments > b.tatalcomments) return 1
+                    if (a.tatalcomments < b.tatalcomments) return -1
+                    return 0
+                })
+            }
+            if (orgMsg.length === 0) {
+                feed.innerHTML = 
+                `
+                <div class="no-feedbacks">
+                    <img src="assets/suggestions/illustration-empty.svg" alt="No Feedbacks">
+                    <h2>There is no feedback yet.</h2>
+                    <p>Got a suggestion? Found a bug that needs to be squashed? We love hearing about new ideas to improve our app.</p>
+                    <div class="btn-primary btn-purple">
+                        <button>+ Add Feedback</button>
+                    </div>
+                </div>
+                `
+            } else {
+                for (let msg of orgMsg) {
+                    if (msg.tatalcomments === 0) {
+                        let ancora = msg.title.replace('&', '%26')
+                        feed.innerHTML += 
+                        `
+                        <div class="comment">
+                            <div class="upvote">
+                                <img src="assets/shared/icon-arrow-up.svg" alt aria-hidden="true">
+                                <p>${msg.upvotes}</p>
+                            </div>
+                            <div class="comment-content">
+                                <h2>${msg.title}</h2>
+                                <p>${msg.description}</p>
+                                <div>
+                                    <div class="category">
+                                        <P>${msg.category}</P>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="feedback-detail.html?title=${ancora}" class="comments">
+                                <img src="assets/shared/icon-comments.svg" alt aria-hidden="true">
+                                <p class="zero-comments">${msg.tatalcomments}</p>
+                            </a>
+                        </div>
+                        `
+                    } else {
+                        let ancora = msg.title.replace('&', '%26')
+                        feed.innerHTML += 
+                        `
+                        <div class="comment">
+                            <div class="upvote">
+                                <img src="assets/shared/icon-arrow-up.svg" alt aria-hidden="true">
+                                <p>${msg.upvotes}</p>
+                            </div>
+                            <div class="comment-content">
+                                <h2>${msg.title}</h2>
+                                <p>${msg.description}</p>
+                                <div>
+                                    <div class="category">
+                                        <P>${msg.category}</P>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="feedback-detail.html?title=${ancora}" class="comments">
+                                <img src="assets/shared/icon-comments.svg" alt aria-hidden="true">
+                                <p>${msg.tatalcomments}</p>
+                            </a>
+                        </div>
+                        `
+                    }
+                }
+            }
+        })
+    })
+}
---
+++ FrontendMentor/product-feedback-app/starter-code/js/index.js
@@ -0,0 +1,26 @@
+import { menu, categorySelector } from "./menu.js"
+import { attFeed } from "./feed.js"
+
+// menu
+const btnOpenMenu = document.querySelector('.btn-open-menu')
+const btnCloseMenu = document.querySelector('.btn-close-menu')
+const asideContent = document.querySelector('.aside-content')
+const html = document.querySelector('html')
+const sort = document.querySelector('.sort-by')
+const sortActive = document.querySelector('.sort-by-active')
+const sortLi = document.querySelectorAll('.sort-by-active ul li')
+const sortByP = document.querySelector('.sort-by p .sort-span')
+const sortArrowUp = document.querySelector('.sort-arrow-up')
+const sortArrowDown = document.querySelector('.sort-arrow-down')
+const feed = document.querySelector('.feed .container-grid')
+var i = 0
+var categoryActive = document.querySelector('.category-active p')
+menu(btnOpenMenu, btnCloseMenu, asideContent, html, sort, sortActive, sortArrowDown, sortArrowUp, i, sortLi, sortByP, feed)
+
+
+// feed
+attFeed(feed, categoryActive.innerHTML, 'Most Upvotes')
+
+// category selector
+const categories = document.querySelectorAll('.category')
+categorySelector(categories, feed, sortByP)
